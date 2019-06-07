d772dd8b178a83fb5648a653015a5741a1395da2
wesselottevanger <wesselott2@gmal.com>
2019-06-07 11:44:25 +0200

    update

--- demo.ino
+++ demo.ino
@@ -1,18 +1,35 @@
-#include <sps30.h>
-//#include <SoftwareSerial.h>
-#include <Adafruit_BluefruitLE_UART.h>
-#include <Adafruit_Si7021.h>
+//SD libraries en objecten
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
 #include "RTClib.h"
+char filename[] = "LOGGER00.CSV";
+DateTime now;
+String Time;
+#define SYNC_INTERVAL 1000
+#define WAIT_TO_START 0
+RTC_DS1307 RTC;
+File logfile;
+uint32_t syncTime = 0;
+
+//Bluetooth libraries en objecten
+#include <Adafruit_BluefruitLE_UART.h>
 #include "BluefruitConfig.h"
 #define FACTORYRESET_ENABLE         0
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 #define MODE_LED_BEHAVIOUR          "MODE"
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN, BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+//fijnstof libraries en objecten
+#include <sps30.h>
+struct sps30_measurement measurement;
+
+//temp&humidity  libraries en objecten
+#include <Adafruit_Si7021.h>
+Adafruit_Si7021 sensor = Adafruit_Si7021();
 
 #define debug
-
 #ifdef debug
 #define LOG(...) Serial.print(__VA_ARGS__)
 #define LOGLN(...) Serial.println(__VA_ARGS__)
@@ -21,50 +38,26 @@
 #define LOGLN(...)
 #endif
 
-Adafruit_Si7021 sensor = Adafruit_Si7021();
-
-#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
-uint32_t syncTime = 0;     // time of last sync()
-
-#define WAIT_TO_START 0 // Wait for serial input in setup()
-
-RTC_DS1307 RTC; // define the Real Time Clock object
-
-// the logging file
-File logfile;
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-
-Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
-                              BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
-
-char filename[] = "LOGGER00.CSV";
-DateTime now;
-String Time;
-struct sps30_measurement measurement;
-
-void error(String str)
+void error(const char* str)
 {
-  LOG(F("error: "));
-  LOGLN(str);
+  LOGLN("error: " + str);
   while (1);
 }
 
 void setup(void)
 {
-  Serial.begin(115200);
-#if WAIT_TO_START
-  LOG(F("Type any character to start\n"));
+  Serial.begin(9600);
+  //wacht tot seriele port beschikbaar is.
+  #if WAIT_TO_START
+  LOGLN(F("Type any character to start"));
   while (!Serial.available());
-#endif
-  LOG(F("Initializing SD card..."));
+  #endif
+
+  //Initialiseer SD kaart
+  LOGLN(F("Initializing SD card..."));
   pinMode(10, OUTPUT);
   if (!SD.begin(10)) error(F("Card failed, or not present"));
-  LOG(F("card initialized.\n"));
-  if (!sensor.begin()) error(F("Did not find Si7021 sensor!"));
-  while  (sps30_probe() != 0) LOG(F("Could not probe sps30!\n"));
-  if  (sps30_start_measurement() != 0) error(F("Could not start sps30 measurement!"));
-  delay(10000);
-  
+  LOGLN(F("card initialized."));
   Wire.begin();
   if (!RTC.begin()) error(F("RTC failed"));
   for (uint8_t i = 0; i < 100; i++)
@@ -73,9 +66,8 @@ void setup(void)
     filename[7] = i % 10 + '0';
     if (!SD.exists(filename))
     {
-      // only open a new file if it doesn't exist
       logfile = SD.open(filename, FILE_WRITE);
-      break; // leave the loop!
+      break;
     }
   }
   if (!logfile) error(F("couldnt create file"));
@@ -83,38 +75,25 @@ void setup(void)
   LOGLN(filename);
   analogReference(EXTERNAL);
 
-  if ( !ble.begin(VERBOSE_MODE) ) error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  LOGLN( F("OK!") );
+  //Initialiseer SI7021 sensor
+  if (!sensor.begin()) error(F("Did not find Si7021 sensor!"));
 
-  if (FACTORYRESET_ENABLE)
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    LOGLN(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ) error(F("Couldn't factory reset"));
-  }
+  //Initialiseer sps30 sensor
+  while  (sps30_probe() != 0) LOGLN(F("Could not probe sps30!"));
+  if  (sps30_start_measurement() != 0) error(F("Could not start sps30 measurement!"));
+  delay(10000);
 
-  /* Disable command echo from Bluefruit */
-  ble.echo(false);
-
-  LOGLN(F("Requesting Bluefruit info:"));
-  /* Print Bluefruit information */
-  ble.info();
-  ble.verbose(false);  // debug info is a little annoying after this point!
-
-  LOGLN(F("******************************"));
-
-  // LED Activity command is only supported from 0.6.6
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
-  {
-    // Change Mode LED Activity
-    LOGLN(F("Change LED activity to " MODE_LED_BEHAVIOUR));
-    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
-  }
-
-  // Set module to DATA mode
-  LOGLN( F("Switching to DATA mode!") );
-  ble.setMode(BLUEFRUIT_MODE_DATA);
   
+  
+  //Initialiseer Bluetooth
+  if ( !ble.begin(VERBOSE_MODE) ) error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  if (FACTORYRESET_ENABLE) if ( ! ble.factoryReset() ) error(F("Couldn't factory reset"));
+  ble.echo(false);
+  ble.info(); //print bluetooth info
+  ble.verbose(false); //disable debug
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) ) ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR); // Change Mode LED Activity // LED Activity command is only supported from 0.6.6
+  ble.setMode(BLUEFRUIT_MODE_DATA); // Set module to DATA mode
+
 }
 
 
@@ -149,7 +128,7 @@ void loop(void)
   {
     Write(String(sensor.readTemperature()).c_str(), "Temperature");
     Write(String(sensor.readHumidity()).c_str(), "Humidity");
-    //if(ret >= 0) Write(String(measurement.mc_1p0, 4) + ";" + String(measurement.mc_2p5, 4) + ";" + String(measurement.mc_4p0, 4) + ";" + String(measurement.mc_10p0, 4),F("Particulate matter"));
+    if (ret >= 0) Write((String(measurement.mc_1p0, 4) + ";" + String(measurement.mc_2p5, 4) + ";" + String(measurement.mc_4p0, 4) + ";" + String(measurement.mc_10p0, 4)).c_str(), "Particulate matter");
     // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
     // which uses a bunch of power and takes time
     if ((millis() - syncTime) < SYNC_INTERVAL)

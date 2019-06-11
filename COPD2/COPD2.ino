9e82529ba8a9610f02b1552bfba26c211b432c6f
jobvanellen <jobvanellen@kpnmail.nl>
2019-06-11 14:53:26 +0200

    infite loop at gps fail removed

--- COPD2/COPD2.ino
+++ COPD2/COPD2.ino
@@ -86,59 +86,7 @@ void loop() {
 
     if (millis() > 5000 && gpsChars() < 10){
       Serial.println(F("No GPS detected: check wiring."));
-      while(true);
     }
     Time = millis();
   }
 }
-
-/*void GPSInfo()
-{
-  Serial.print(F("Location: "));
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() + 2 < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour() + 2);
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println();
-}*/
